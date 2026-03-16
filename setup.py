c05717cee8b0242f9c4c1e61d50ae19ad8fd85ef
maxime-rousselet-cnes <maxime.rousselet@cnes.fr>
2026-03-16 13:51:24 +0100

    Minor adjustment

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+"""
+To install via pip install -e . in the root of the repository. This will make the BASE_MODELS
+package available in the current environment.
+"""
+
 from setuptools import find_packages, setup
 
 setup(
     name="BASE_MODELS",
     packages=find_packages(),
     version="0.0.1",
-    description="Base models library needed for a few scientific computing libraries",
+    description="Base models library needed for a few scientific computing libraries.",
     author="Maxime Rousselet",
 )
